13b518e2b6d59eb8934d5bfd44240c858a1a2663
Djam1977 <117649014+Djam1977@users.noreply.github.com>
2023-03-02 11:19:51 +0100

    quetesept

---
+++ Hangar.java
@@ -0,0 +1,13 @@
+public class Hangar {
+
+ public static void main(String[] args)  { 
+
+  Car renault = new Car("Renault");
+
+
+ System.out.println(renault.doStuff());
+
+Boat zodiac = new Boat("Zodiac");
+
+System.out.println(zodiac.doStuff());
+}}
